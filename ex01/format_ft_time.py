85c65f365a5aa0972ccd55c2dc249c4c8187019a
Mohamed El hadjaoui <mel-hadj@e1r9p3.1337.ma>
2024-03-25 03:26:15 +0000

    ex01 done , ex02 WIP

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,4 +1,5 @@
 import datetime
 
 now = datetime.datetime.now()
-print(now.strftime("%Y-%m-%d %H:%M:%S"))
+print('Seconds since January 1, 1970: {:,.4f}'.format(now.timestamp()), 'or', format(now.timestamp(), 'e'), 'in scientific notation') # https://www.w3schools.com/python/ref_string_format.asp
+print(now.strftime('%b %m %Y') ) # https://www.w3schools.com/python/python_datetime.asp
