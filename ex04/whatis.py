6fc70d7b60a13910ddae755d182395065b873e81
Mohamed El hadjaoui <mel-hadj@e1r6p12.1337.ma>
2024-03-30 16:09:01 +0000

    ex04 done

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,18 +1,18 @@
-import argparse
+
 import sys
 
-parser = argparse.ArgumentParser(exit_on_error=False)
-parser.add_argument('integers', type = int, nargs = 1)
-try:
-    args = parser.parse_args(sys.argv[1:])
-    if len(args.integers) > 1:
+def main():
+    if len(sys.argv) > 2:
         print("AssertionError: more than one argument is provided")
-        sys.exit(1)
-    object = args.integers[0]
-    if object % 2 == 0:
-        print("I'm Even.")
-    else:
-        print("I'm Odd.")
-except (argparse.ArgumentError) as e:
-    print(e.__class__.__name__)
-    print("AssertionError: argument is not an integer")
+        return
+    elif len(sys.argv) < 2:
+        return
+    try:
+        number = int(sys.argv[1])
+    except ValueError:
+        print("AssertionError: argument is not an integer")
+        return
+    print("I'm Even." if number % 2 == 0 else "I'm Odd.")
+
+if __name__ == "__main__":
+    main()
