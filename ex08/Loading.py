cd9f86483f461a8bb91aa0cf58c61ce4d3aec215
elhadjaoui <achraf-2060@outlook.fr>
2024-05-11 13:45:37 +0100

    WIP

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -1,18 +1,38 @@
+from time import sleep
 
-import time
-import sys
+
+def ft_tqdm(lst: range) -> None:
+    """Print the progress of the list processing"""
+    for i in lst:
+        yield i
 
 
 def main():
-    """ simulate the loading like tqdm """
-
-    for i in range(100):
-        time.sleep(0.1)
-        sys.stdout.write("\r[%-100s] %d%%" % ('=' * i, i))
-        sys.stdout.flush()
-    print()
+    lst = range(1000)
+    for i in ft_tqdm(lst):
+        percentage = (i*100)/len(lst)
+        spaces = " " * (int(percentage) - i)
+        print(f"\r{percentage:.2f}%|[{'='* int(percentage)}{spaces}]", end="")
+        sleep(0.1)
 
 
 if __name__ == "__main__":
     """Run the main function"""
     main()
+
+
+
+# for i in range(21):
+#     spaces = " " * (20 - i)
+#     percentage = 5*i
+#     print(f"\r[{'='*i}{spaces}]{percentage}%", flush=True, end="")
+#     sleep(0.25)
+   
+    # countdown.py
+
+print(f"{'Countdown':^20}")
+
+# for second in range(3, 0, -1):
+#     print(second, flush=True, end=" ")
+#     sleep(1)
+# print("Go!")
