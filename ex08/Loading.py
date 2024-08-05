157d4236b539d476e4e9300e78b58496a0504870
elhadjaoui <achraf-2060@outlook.fr>
2024-08-05 14:00:03 +0100

    done with ex08

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -1,18 +1,24 @@
-from time import sleep
-
 
 def ft_tqdm(lst: range) -> None:
     """Print the progress of the list processing"""
     for i in lst:
+        n = len(lst)
+        percentage = (i*100)/(len(lst))
+        space_to_fill = (((n - i)*100)//n)
+        spaces = ' ' * space_to_fill
+        print(f"\r{percentage:.0f}%|{'█'* int(percentage)}{spaces}| ", end="")
+        print(f"{i + 1}/{n}", end="")
         yield i
 
 
 def main():
-    lst = range(1000)
+    lst = range(100)
+    # for i in tqdm(range(10000)):
+    #     sleep(0.01)
     for i in ft_tqdm(lst):
         percentage = (i*100)/len(lst)
-        spaces = " " * (int(percentage) - i)
-        print(f"\r{percentage:.2f}%|[{'='* int(percentage)}{spaces}]", end="")
+        spaces = ' ' * (int(percentage) - i)
+        print(f"\r{percentage:.2f}%|[{'█'* int(percentage)}{spaces}]", end="")
         sleep(0.1)
 
 
@@ -20,19 +26,3 @@ if __name__ == "__main__":
     """Run the main function"""
     main()
 
-
-
-# for i in range(21):
-#     spaces = " " * (20 - i)
-#     percentage = 5*i
-#     print(f"\r[{'='*i}{spaces}]{percentage}%", flush=True, end="")
-#     sleep(0.25)
-   
-    # countdown.py
-
-print(f"{'Countdown':^20}")
-
-# for second in range(3, 0, -1):
-#     print(second, flush=True, end=" ")
-#     sleep(1)
-# print("Go!")
